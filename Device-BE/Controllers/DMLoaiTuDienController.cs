c5b687db7df015ee45284adf0038895d41e4754f
cuongnb3698 <cuongnb3698@gmail.com>
2020-12-07 23:28:09 +0700

    Add project files.

---
+++ Device-BE/Controllers/DMLoaiTuDienController.cs
@@ -0,0 +1,76 @@
+﻿using Device_BE.Models;
+using Device_BE.Models.MDevice;
+using Device_BE.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device_BE.Controllers
+{
+    [Route("api/loaitudien")]
+    [ApiController]
+    public class DMLoaiTuDienController : ControllerBase
+    {
+        private readonly DeviceContext _context;
+        public DMLoaiTuDienController(DeviceContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<LoaiTuDienModel> getPage(string sSearch,string ma)
+        {
+            var data = _context.CMLoaiTuDiens;
+            var query = from list in data
+                        select list;
+            if (!String.IsNullOrEmpty(sSearch))
+            {
+                query = query.Where(x => x.Ten.Contains(sSearch));
+            }
+            if (!String.IsNullOrEmpty(ma))
+            {
+                query = query.Where(x => x.MaLoai == ma);
+            }
+            return query.Select(x => new LoaiTuDienModel
+            {
+                Id = x.Id,
+                MaLoai = x.MaLoai,
+                TenLoai = x.Ten
+            }).ToList();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult<CMLoaiTuDien> Create(CMLoaiTuDien model)
+        {
+            _context.CMLoaiTuDiens.Add(model);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPut]
+        public ActionResult<CMLoaiTuDien> Update(CMLoaiTuDien model)
+        {
+            _context.Entry(model).State = EntityState.Modified;
+            _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [Route("{id}")]
+        [HttpDelete]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var delete = await _context.CMLoaiTuDiens.FindAsync(id);
+            if (delete == null)
+            {
+                return Ok("Xóa Không thành công");
+            }
+            _context.CMLoaiTuDiens.Remove(delete);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Device-BE/Database/MDevice/CMTuDien.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Device_BE.Models.MDevice
+{
+    public class CMTuDien
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string MaTuDien { get; set; }
+        [Required]
+        [ForeignKey("IdLoaiTuDien")]
+        public CMLoaiTuDien CMLoaiTuDien { get; set; }
+        [StringLength(50)]
+        public string TenNgan { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Ten { get; set; }
+        [StringLength(100)]
+        public string GhiChu { get; set; }
+        public int UuTien { get; set; }
+        public bool Active { get; set; }
+
+
+
+
+    }
+}
